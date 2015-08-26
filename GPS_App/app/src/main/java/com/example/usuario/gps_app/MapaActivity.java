d7cd3a7b9f93bb79113f66a687380e4bbf3a79d4
cristobalgc <colon981@hotmail.com>
2015-08-26 12:18:27 +0200

    Codigo fuente Android eliminado

--- GPS_App/app/src/main/java/com/example/usuario/gps_app/MapaActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.usuario.gps_app;
-
-import com.google.android.gms.maps.MapFragment;
-
-/**
- * Created by usuario on 27/04/2015.
- */
-public class MapaActivity extends MapFragment {
-}
